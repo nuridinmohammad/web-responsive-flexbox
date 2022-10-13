d7ded9b2f84ab5967000f0be9495922e0dcaf5fd
Mohammad Nuridin <developer.mohammadnuridin@gmail.com>
2022-10-13 09:04:32 +0700

    setup project

---
+++ v1/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.js",
+  mode: "development",
+  watch: true,
+  devtool: false,
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
